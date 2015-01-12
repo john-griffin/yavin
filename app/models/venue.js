69029e999d1f6c2f216bbc362b36bf025d65b75c
John Griffin <johnog@gmail.com>
2015-01-12 15:59:28 -0500

    add fields to venues

--- app/models/venue.js
+++ app/models/venue.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   stops: DS.hasMany('stop'),
+  description: DS.attr('string'),
+  photoUrl: DS.attr('string'),
+  location: DS.attr('string'),
   name: DS.attr('string')
 });

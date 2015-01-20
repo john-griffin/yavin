8e5e4c06dfadcd84836ea06bcee3a4c83bfb658d
John Griffin <johnog@gmail.com>
2015-01-20 16:05:06 -0500

    unneeded action

--- app/routes/stops/new/venue-show.js
+++ app/routes/stops/new/venue-show.js
@@ -1,12 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    saveStop: function(){
-      console.log("venue show route");
-      return true;
-    }
-  },
   model: function(params) {
     return this.store.fetch('fq-venue', params.venue_id);
   }

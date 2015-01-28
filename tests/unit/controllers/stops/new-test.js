0abb777f8325919c0f196d3eef747c447782adf0
John Griffin <johnog@gmail.com>
2015-01-27 21:14:14 -0500

    get tests passing

--- tests/unit/controllers/stops/new-test.js
+++ tests/unit/controllers/stops/new-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleFor('controller:stops/new', 'StopsNewController', {
   // Specify the other units that are required for this test.
-  needs: ['controller:venues/show']
+  needs: ['controller:venues/new/show']
 });
 
 // Replace this with your real tests.

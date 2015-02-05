08ed65c51ac05eb55fcf8703b16d7647a1ca476c
John Griffin <johnog@gmail.com>
2015-02-05 11:39:14 -0500

    stop add button, use component to change context

--- app/controllers/stops/index.js
+++ app/controllers/stops/index.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   needs: ['crawls/show'],
+  crawl: Ember.computed.alias('controllers.crawls/show'),
   isOwner: Ember.computed('session.id', function(){
-    return this.get("session.id") === this.get('controllers.crawls/show.userId');
+    return this.get("session.id") === this.get('crawl.userId');
   }),
   sortProperties: ['rowOrder']
 });

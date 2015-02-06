a3c176169ce3482a45f393b0434a6a0347157735
John Griffin <johnog@gmail.com>
2015-02-06 11:20:26 -0500

    clean up dirty crawls

--- app/routes/crawls/base.js
+++ app/routes/crawls/base.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import AuthRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
+import DataRoute from 'ember-data-route/mixins/data-route';
 
-export default Ember.Route.extend(AuthRouteMixin, {
+export default Ember.Route.extend(AuthRouteMixin, DataRoute, {
   actions: {
     save: function(crawl){
       var _this = this;

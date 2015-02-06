ef37173a0e680508560ce5f2b37b7ddcc4db99e8
John Griffin <johnog@gmail.com>
2015-02-06 15:55:47 -0500

    limit to nightlife and food categories

--- app/routes/venues/index.js
+++ app/routes/venues/index.js
@@ -18,6 +18,8 @@ export default Ember.Route.extend({
     if (params.query) {
       var city = this.modelFor('crawls.show').get('city');
       params.near = city;
+      // limit to 'Nightlife Spots' and 'Food'
+      params.categoryId = '4d4b7105d754a06376d81259,4d4b7105d754a06374d81259';
       venues = this.store.find('fq-venue', params);
     }
     return venues;

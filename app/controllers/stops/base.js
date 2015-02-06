8012e381a0ab9f4e1a3720ec577aae29c8d31eb3
John Griffin <johnog@gmail.com>
2015-02-06 13:58:08 -0500

    disable form saving when invalid

--- app/controllers/stops/base.js
+++ app/controllers/stops/base.js
@@ -4,6 +4,8 @@ import EmberValidations from 'ember-validations';
 export default Ember.Controller.extend(EmberValidations.Mixin, {
   needs: ['crawls/show'],
   crawl: Ember.computed.alias('controllers.crawls/show'),
+  isAllValid: Ember.computed.and('isValid', 'venueController.isValid'),
+  isAllInvalid: Ember.computed.not('isAllValid'),
   validations: {
     "model.name": {
       presence: true

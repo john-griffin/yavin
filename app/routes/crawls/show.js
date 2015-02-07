861b3c72fe03b134a8681e2181af85291db28e83
John Griffin <johnog@gmail.com>
2015-02-06 19:06:37 -0500

    handle 404 on api requests

--- app/routes/crawls/show.js
+++ app/routes/crawls/show.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    error: function() {
+      this.transitionTo('/fourOhFour');
+    }
+  },
   afterModel: function(resolvedModel, transition) {
     if (transition.targetName === 'crawls.show.index') {
       this.transitionTo('stops');

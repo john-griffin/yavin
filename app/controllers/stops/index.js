81209b4c5ffe3e0618f7e4ff36fc2d498ead8ceb
John Griffin <johnog@gmail.com>
2015-02-10 16:48:59 -0500

    property was getting cached

--- app/controllers/stops/index.js
+++ app/controllers/stops/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   needs: ['crawls/show'],
   crawl: Ember.computed.alias('controllers.crawls/show'),
-  isOwner: Ember.computed('session.id', function(){
+  isOwner: Ember.computed('session.id', 'crawl.userId', function(){
     return this.get("session.id") === this.get('crawl.userId');
   }),
   isEmpty: Ember.computed.empty('model'),

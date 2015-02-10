b0367e08a1b8683694b31eea98419417b1f5d03f
John Griffin <johnog@gmail.com>
2015-02-10 16:58:26 -0500

    empty crawl list message

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,5 +4,6 @@ export default Ember.Controller.extend({
   featuredCrawls: Ember.computed.filterBy('model', 'featured', true),
   ownedCrawls: Ember.computed.filter('model', function(crawl){
     return crawl.get('userId') === this.get('session.id');
-  })
+  }),
+  emptyOwnedCrawls: Ember.computed.empty('ownedCrawls')
 });

5205c5df2c97b55463d7e69d05237da52e878d13
John Griffin <johnog@gmail.com>
2015-02-12 18:03:01 -0500

    ensure computed properties are triggered

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  featuredCrawls: Ember.computed.filterBy('model', 'featured', true),
+  featuredCrawls: Ember.computed.filterBy('model', 'featured', true).property('model.@each.featured'),
   ownedCrawls: Ember.computed.filter('model', function(crawl){
     return crawl.get('userId') === this.get('session.id');
-  }),
+  }).property('model.@each.userId', 'session.id'),
   emptyOwnedCrawls: Ember.computed.empty('ownedCrawls')
 });

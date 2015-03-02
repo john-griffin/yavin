ccb59670ea1c6aa31c73d46a09ed332ddaa4471a
John Griffin <johnog@gmail.com>
2015-03-02 15:57:18 -0500

    tidy venue serializer

--- app/serializers/fq-venue.js
+++ app/serializers/fq-venue.js
@@ -2,13 +2,13 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
-    categories: {embedded: 'always'},
-    photos: {embedded: 'always'}
+    categories: { embedded: 'always' },
+    photos: { embedded: 'always' }
   },
   extractFind: function(store, type, payload, id, requestType) {
     payload = { "fq-venue": payload.response.venue };
-    if (payload["fq-venue"]["photos"].count > 0) {
-      payload["fq-venue"]['photos'] = payload["fq-venue"]["photos"]['groups'][0]["items"];
+    if (payload["fq-venue"].photos.count > 0) {
+      payload["fq-venue"].photos = payload["fq-venue"].photos.groups[0].items;
     }
     return this._super(store, type, payload, id, requestType);
   },
